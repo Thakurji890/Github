bbebdb396ef2b0e3becaac17bfaaa6e28b512af0
Thakurji890 <tha.rupesh456@gmail.com>
2026-05-31 22:04:51 +0530

    edit : edit for Credentials

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -1,9 +1,18 @@
-const { S3Client } = require("@aws-sdk/client-s3");
+require("dotenv").config();
 
-const s3 = new S3Client({ region: "ap-south-1" });
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
+const s3 = new S3Client({
+  region: process.env.REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+});
 const S3_BUCKET = "github-buckets3";
 
 module.exports = {
   s3,
   S3_BUCKET,
+  PutObjectCommand,
 };
