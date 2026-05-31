2f869308aebd70d405b2299b116cc3fb6b5a2b7c
Thakurji890 <tha.rupesh456@gmail.com>
2026-05-31 18:05:50 +0530

    feature: dd feature added

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -1,5 +1,19 @@
-async function addRepo() {
-  console.log("add Command called");
+const fs = require("fs").promises;
+const path = require("path");
+
+async function addRepo(filePath) {
+  const repoPath = path.resolve(process.cwd(), ".fileGit");
+  const stagingPath = path.join(repoPath, "staging");
+
+  try {
+    await fs.mkdir(stagingPath, { recursive: true });
+    const fileName = path.basename(filePath);
+    await fs.copyFile(filePath, path.join(stagingPath, fileName));
+
+    console.log(`file ${fileName} added to staging area!`);
+  } catch (err) {
+    console.error("Error on adding file", err);
+  }
 }
 
 module.exports = { addRepo };
