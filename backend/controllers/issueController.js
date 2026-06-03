c40f1f3742313670a37e4a55879a511690af1184
Thakurji890 <tha.rupesh456@gmail.com>
2026-06-03 19:58:57 +0530

    feat(backend): update issue controllers and add issue router

---
+++ backend/controllers/issueController.js
@@ -0,0 +1,27 @@
+const createIssue = (req, res) => {
+  res.send("Issue Created");
+};
+
+const updateIssueById = (req, res) => {
+  res.send("Issue updated!");
+};
+
+const deleteIssueById = (req, res) => {
+  res.send("Issue Deleted!");
+};
+
+const getAllIssues = (req, res) => {
+  res.send("All Issue Fetched");
+};
+
+const getIssueById = (req, res) => {
+  res.send("Issue Fetched");
+};
+
+module.exports = {
+  createIssue,
+  updateIssueById,
+  deleteIssueById,
+  getAllIssues,
+  getIssueById,
+};
--- backend/routes/main.router.js
+++ backend/routes/main.router.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mainRouter = express.Router();
 
-const userRouter = require("../routes/user.router");
-
-const repoRouter = require("../routes/repo.router");
+const userRouter = require("./user.router");
+const repoRouter = require("./repo.router");
+const issueRouter = require("./issue.router");
 
 mainRouter.use(userRouter);
 mainRouter.use(repoRouter);
+mainRouter.use(issueRouter);
 
 mainRouter.get("/", (req, res) => {
   res.json({ Status: "Good" });
