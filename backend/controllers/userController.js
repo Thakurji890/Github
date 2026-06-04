24efdc37a6877da76e400454714cf01ba59a85ad
Thakurji890 <tha.rupesh456@gmail.com>
2026-06-04 19:29:32 +0530

    feat : install jwt,bcrypt,mongodb and added signup logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,68 @@
-const getAllUsers = (req, res) => {
-  res.send("All user fetched");
-};
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
-const signup = (req, res) => {
-  res.send("Signing Up!");
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+
+async function connectClient() {
+  if (!client) {
+    client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+  }
+}
+
+// Signup logic
+
+const signup = async (req, res) => {
+  const { username, password, email } = req.body;
+  try {
+    await connectClient();
+    const db = client.db(process.env.DB_NAME);
+    const usersCollection = db.collection("users");
+
+    const user = await usersCollection.findOne({ username });
+    if (user) {
+      return res.status(400).json({ message: "User Already exists!" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = {
+      username,
+      password: hashedPassword,
+      email,
+      repositories: [],
+      follewedUsers: [],
+      starRepos: [],
+    };
+
+    const result = await usersCollection.insertOne(newUser);
+
+    const token = jwt.sign(
+      { id: result.insertedId },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "7d" },
+    );
+
+    res.json({ token });
+  } catch (error) {
+    console.error(`Connection Failed Due to ${error.message}`);
+    res.status(500).send("Server Error");
+  }
 };
 
 const login = (req, res) => {
   res.send("Login user!");
+};
+
+const getAllUsers = (req, res) => {
+  res.send("All user fetched");
 };
 
 const getUserProfile = (req, res) => {
