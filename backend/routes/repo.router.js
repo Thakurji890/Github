4496f803212f23467c7bd31f4b22ca611b80c1c1
Thakurji890 <tha.rupesh456@gmail.com>
2026-06-05 21:41:13 +0530

    refactor(backend): update repository routes for name and user lookups

--- backend/routes/repo.router.js
+++ backend/routes/repo.router.js
@@ -6,8 +6,11 @@ const repoController = require("../controllers/repoController");
 repoRouter.post("/repo/create", repoController.creatRepository);
 repoRouter.get("/repo/all", repoController.getAllRepository);
 repoRouter.get("/repo/:id", repoController.fetchRepositoryById);
-repoRouter.get("/repo/:name", repoController.fetchRepositoryByName);
-repoRouter.get("/repo/:userID", repoController.fectchRepositoryForCurrentUser);
+repoRouter.get("/repo/name/:name", repoController.fetchRepositoryByName);
+repoRouter.get(
+  "/repo/user/:userID",
+  repoController.fectchRepositoryForCurrentUser,
+);
 repoRouter.put("/repo/update/:id", repoController.updateRepositoryById);
 repoRouter.delete("/repo/delete/:id", repoController.deleteRepositoryById);
 repoRouter.patch("/repo/toggle/:id", repoController.toggleVisibilityById);
