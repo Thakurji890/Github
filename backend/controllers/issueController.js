22f719b2faf21f1cff21863e46954e6fa46d3c8d
Thakurji890 <tha.rupesh456@gmail.com>
2026-06-07 19:05:32 +0530

    feat : added issue related features- create,update,delete,all issues, issue bby id

--- backend/controllers/issueController.js
+++ backend/controllers/issueController.js
@@ -1,21 +1,90 @@
-const createIssue = (req, res) => {
-  res.send("Issue Created");
+const mongoose = require("mongoose");
+const Repository = require("../models/repoModel");
+const User = require("../models/userModel");
+const Issue = require("../models/issueModel");
+
+const createIssue = async (req, res) => {
+  const { title, description } = req.body;
+  const { id } = req.params;
+
+  try {
+    const issue = new Issue({ title, description, repository: id });
+    await issue.save();
+
+    res.status(201).json(issue);
+  } catch (error) {
+    console.error(`Unable to Create Issue Due to ${error}`);
+    res.status(500).send("Server Error");
+  }
 };
 
-const updateIssueById = (req, res) => {
-  res.send("Issue updated!");
+const updateIssueById = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, status } = req.body;
+  try {
+    const issue = await Issue.findById(id);
+
+    if (!issue) {
+      return res.status(404).json({ message: "Issue Not Found!" });
+    }
+
+    issue.title = title;
+    issue.description = description;
+    issue.status = status;
+
+    await issue.save();
+
+    res.json({ message: "Issue Updated!" }, issue);
+  } catch (error) {
+    console.error(`Unable to Update Issue Due to ${error}`);
+    res.status(500).send("Server Error");
+  }
 };
 
-const deleteIssueById = (req, res) => {
-  res.send("Issue Deleted!");
+const deleteIssueById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const issue = await Issue.findByIdAndDelete(id);
+    if (!issue) {
+      return res.status(404).json({ message: "Issue Not Found!" });
+    }
+
+    res.json({ message: "Issue Deleted!" });
+  } catch (error) {
+    console.error(`Unable to Delete Issue Due to ${error}`);
+    res.status(500).send("Server Error");
+  }
 };
 
-const getAllIssues = (req, res) => {
-  res.send("All Issue Fetched");
+const getAllIssues = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const issues = await Issue.find({ repository: id });
+
+    if (!issues) {
+      return res.status(404).json({ message: "Issue Not Found!" });
+    }
+    res.status(200).json(issues);
+  } catch (error) {
+    console.error(`Unable Fetching Issue Due to ${error}`);
+    res.status(500).send("Server Error");
+  }
 };
 
-const getIssueById = (req, res) => {
-  res.send("Issue Fetched");
+const getIssueById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const issue = await Issue.findById(id);
+    if (!issue) {
+      return res.status(404).json({ message: "Issue Not Found!" });
+    }
+
+    res.send(issue);
+  } catch (error) {
+    console.error(`Unable Fetching Issue Due to ${error}`);
+    res.status(500).send("Server Error");
+  }
 };
 
 module.exports = {
