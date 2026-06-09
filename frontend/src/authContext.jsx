b92efe1918ae5da79f602da0ed4549ee76b91440
Thakurji890 <tha.rupesh456@gmail.com>
2026-06-09 19:17:47 +0530

    feat : added authprovide custom hook

--- frontend/src/authContext.jsx
+++ frontend/src/authContext.jsx
@@ -1,3 +1,25 @@
 import React, { createContext, useState, useEffect, useContext } from "react";
 
+// Creating Context
 const AuthContext = createContext();
+
+// custom hook
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+// if the user logedd in or not
+export const AuthProvider = ({ children }) => {
+  const [currUser, setCurrUser] = useState(null);
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    if (userId) {
+      setCurrUser(userId);
+    }
+  }, []);
+  const value = {
+    currUser,
+    setCurrUser,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
