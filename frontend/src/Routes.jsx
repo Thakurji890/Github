a6e52685dfa57a726597917776ccffd1acfba6f2
Thakurji890 <tha.rupesh456@gmail.com>
2026-06-10 21:58:13 +0530

    feat : added routing through components

---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from "react";
+import { useNavigate, useRoutes } from "react-router-dom";
+
+// pages list
+import Dashboard from "./components/dashboard/Dashoard";
+import Profile from "./components/user/Profile";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+
+// Auth context
+import { useAuth } from "./authContext";
+
+const ProjectRoutes = () => {
+  const { currUser, setCurrUser } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userIdFromStorage = localStorage.getItem("userId");
+    if (userIdFromStorage && !currUser) {
+      setCurrUser(userIdFromStorage);
+    }
+    if (
+      userIdFromStorage &&
+      !["/auth", "signup"].includes(window.location.pathname)
+    ) {
+      navigate("/auth");
+    }
+    if (userIdFromStorage && window.location.pathname == "/auth") {
+      navigate("/");
+    }
+  }, [currUser, navigate, setCurrUser]);
+
+  let element = useRoutes([
+    {
+      path: "/",
+      element: <Dashboard />,
+    },
+    {
+      path: "/auth",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+    {
+      path: "/profile",
+      element: <Profile />,
+    },
+  ]);
+
+  return element;
+};
+
+export default ProjectRoutes;
